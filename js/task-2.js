9bdee7236fce659f2ff866b65339a6cd13bc301c
MagdalenaAntkowiak <130896720+MagdalenaAntkowiak@users.noreply.github.com>
2024-04-22 20:39:08 +0200

    html outside js

--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,8 @@
-<script>function getShippingMessage (country, price, deliveryFee){
-    const totalPrice=price + deliveryFee;
-    return `Shipping to ${country} will cost ${totalPrice} credits`}
+function getShippingMessage(country, price, deliveryFee) {
+  const totalPrice = price + deliveryFee;
+  return `Shipping to ${country} will cost ${totalPrice} credits`;
+}
 
-
-
-
-
-    console.log(getShippingMessage("Australia", 120, 50)); 
-    console.log(getShippingMessage("Germany", 80, 20)); 
-    console.log(getShippingMessage("Sweden", 100, 20)); 
-    
-</script>;
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Sweden", 100, 20));
